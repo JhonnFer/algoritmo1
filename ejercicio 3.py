603c4e4ed9df67cb6c8861b4fe739931ee184fd1
JhonnFer <jhoncasq23@gmail.com>
2024-05-15 12:04:34 -0500

    Add files via upload
    este es un programa de instagram

---
+++ ejercicio 3.py
@@ -0,0 +1,33 @@
+
+print ("instagram")
+
+user = input("phone numbre, username or email")
+password= input("password")
+ 
+if user == "you" and password=="1234":
+    print(f'welcome {user}')
+    print("loanding your posts")
+else:
+    print("lamentamos, you password es incorrecto.vuelve a ingresar correctamente")
+
+
+print("sing uo to see photos and videos from your friends.")
+input("log in with Facebook")
+facebook=input("si o no")
+if facebook=="si":
+    print("welcome")
+else:
+    mobile=input("mobile number or email")
+    name=input("full name")
+    username=input("username")
+    password=input("password")
+    
+
+
+
+
+
+
+
+
+
